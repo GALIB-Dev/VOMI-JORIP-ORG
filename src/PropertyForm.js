378519dac726ccd4e256976ddd26ef46b6f6fca3
Galib <mohammadalgalib@gmail.com>
2024-10-24 11:57:53 +0600

    add updated Navbar.css, Home.js, Home.css, Contact.css,add updated src folder

--- src/PropertyForm.js
+++ src/PropertyForm.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import './Contact.css'; // Assuming you have a Contact.css for styling
 
-const PropertyForm = () => {
-  const googleFormLink = "https://docs.google.com/forms/d/e/1FAIpQLSdGqM0iqdW3d3G2V7b_2w9aEQCnuIUJGXnP85b8VnuZId00eg/viewform?usp=sf_link"; // Replace with your actual Google Form link
-
+const Contact = () => {
   return (
-    <div className="form-container">
-      <h2>Submit Property Information</h2>
-      <p>Please fill out the form below to submit your property information:</p>
-      <a 
-        href={googleFormLink} 
-        target="_blank" 
-        rel="noopener noreferrer" 
-        className="form-link"
-      >
-        Open Property Form
-      </a>
+    <div className="contact-container">
+      <div className="contact-header">
+        <h1>আমাদের অফিস: জয়পুরহাট সদর</h1>
+        <p></p>
+        <h3>ফোন নম্বর: <a href="tel:+8801712617226">+8801712617226</a></h3>
+      </div>
+
+      <div className="form-container">
+        {/* Embed Google Form using iframe */}
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdGqM0iqdW3d3G2V7b_2w9aEQCnuIUJGXnP85b8VnuZId00eg/viewform?embedded=true"
+          width="100%"
+          height="600"
+          frameBorder="0"
+          marginHeight="0"
+          marginWidth="0"
+          title="Contact Form"
+        >
+          লোড হচ্ছে...
+        </iframe>
+      </div>
     </div>
   );
 };
 
-export default PropertyForm;
+export default Contact;

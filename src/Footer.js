386e562bfa3db9f6f8af27ff276dd9946e16b05c
Galib <mohammadalgalib@gmail.com>
2024-11-19 00:09:06 +0600

    Resolved

--- src/Footer.js
+++ src/Footer.js
@@ -5,14 +5,32 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>
-        Powered by <a href="https://12bases.xyz" target="_blank" rel="noopener noreferrer">          <span style={{ color: 'red' }}> <b>Ⅻ</b></span> 𝘽𝘼𝙎𝙀𝙎
+        Powered by{' '}
+        <a
+          href="https://12bases.xyz"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link"
+        >
+          <span style={{ color: 'red' }}>
+            <b>Ⅻ</b>
+          </span>{' '}
+          𝘽𝘼𝙎𝙀𝙎
         </a>
       </p>
-
-
+      <p>
+        <a
+          href="/privacy-policy.html"
+          className="footer-link privacy-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Privacy Policy
+        </a>
+      </p>
       <p>&copy; {new Date().getFullYear()} ভূমি জরিপ উন্নয়ন সংস্থা. All Rights Reserved.</p>
     </footer>
   );
-}
+};
 
 export default Footer;

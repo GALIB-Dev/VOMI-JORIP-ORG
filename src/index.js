a9974d33ca68486c8ffd008269b725375a24d14f
Galib <mohammadalgalib@gmail.com>
2024-11-23 16:02:50 +0600

    Update configuration and add styles

--- src/index.js
+++ src/index.js
@@ -1,51 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
-import { ToastContainer } from 'react-toastify';
-import { ThemeProvider } from './contexts/ThemeContext';
-import { LoadingProvider } from './contexts/LoadingContext';
-import { AuthProvider } from './contexts/AuthContext';
-import ErrorBoundary from './components/common/ErrorBoundary';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/globals.css';
 
-// Global styles
-import './styles/index.css';
-import 'react-toastify/dist/ReactToastify.css';
-
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <HelmetProvider>
-        <ThemeProvider>
-          <AuthProvider>
-            <LoadingProvider>
-              <App />
-              <ToastContainer
-                position="bottom-right"
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-              />
-            </LoadingProvider>
-          </AuthProvider>
-        </ThemeProvider>
-      </HelmetProvider>
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>
 );
-
-// Only register in production
-if (process.env.NODE_ENV === 'production') {
-  serviceWorkerRegistration.register();
-} else {
-  serviceWorkerRegistration.unregister();
-}
